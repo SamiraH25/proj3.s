d71e7c14572a1dbc826374b3a6143f17cde04947
SamiraH25 <samira.harrington@bison.howard.edu>
2026-04-21 08:59:32 -0400

    Update proj3.s
    From Proj 2

--- proj3.s
+++ proj3.s
@@ -4,3 +4,12 @@ substring:    .space 11
 nan_str:      .asciiz "NaN" 
 semi:         .asciiz ";"   
 strint:       .space 4000   # Unsigned integer array space
+
+.text 
+.globl main
+
+main: 
+     li $v0, 8       
+     la $a0, input_buffer  
+     li $a1, 1001        
+     syscall 
